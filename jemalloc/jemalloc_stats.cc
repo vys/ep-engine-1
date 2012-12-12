80e8d6d4ebd3338b3e70d9b9fc7767d676f3ecb6
Vandana Shah <vashah@zynga.com>
2012-12-12 22:53:46 +0530

    Added epoch call to get jemalloc stats refreshed. Without this, the stats are not updated.
    Signed-off-by: Vandana Shah <vashah@zynga.com>

--- jemalloc/jemalloc_stats.cc
+++ jemalloc/jemalloc_stats.cc
@@ -1,10 +1,14 @@
 #include <unistd.h>
+#include <stdint.h>
 #include "jemalloc_stats.hh"
 #include <jemalloc/jemalloc.h>
 
 size_t JemallocStats::getJemallocMapped() {
     size_t param = -1;
     size_t sz_am = sizeof(param);
+    uint64_t epoch = 1;
+    size_t epoch_size = sizeof(epoch);
+    mallctl("epoch", &epoch, &epoch_size, &epoch, sizeof(epoch));
     mallctl("stats.mapped", &param, &sz_am, NULL, 0);
     return param;
 }
