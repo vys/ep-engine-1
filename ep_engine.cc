7c12640b4970d5c30fea17c301de8e85a56510a5
Vinay Y S <vinay@vys.in>
2012-12-13 17:28:41 +0530

    Fix: getl binary-protocol leaks item.
    In binary protocol implementation of getl, it was not freeing the item
    object. This was causing massive leak.
    
    Signed-off-by: Vinay Y S <vinay@vys.in>
    
    Signed-off-by: Vandana Shah <vashah@zynga.com>

--- ep_engine.cc
+++ ep_engine.cc
@@ -989,6 +989,7 @@ extern "C" {
                     static_cast<uint16_t>(res), item->getCas(),
                     cookie);
             }
+            delete item;
         } else if (item) {
             std::string key  = item->getKey();
             uint32_t flags = item->getFlags();
